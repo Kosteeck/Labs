fd66ea7529defa494efb422d4db253bcea2578d5
Kosteeck <lavrukhinkostya@mail.ru>
2017-10-03 20:17:54 +0300

    Lab1

---
+++ Lab1/Lab1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+#include <conio.h>
+#define Span 4.37445
+#define Arsh 1.40607424072
+
+
+int main()
+{
+
+	float meters, arshins, span;
+
+	printf("Please, enter positive number of meters: ");
+	scanf("%f", &meters);
+			if (meters >= 0)
+		{
+			arshins = meters * Arsh;
+			printf("\n Your number of meters in arshins: %f", arshins);
+			span = meters * Span;
+			printf("\n Your number of meters in spans: %f", span);
+		}
+		else
+		{
+			printf("invalid input \n");
+
+		}
+			_getch();
+		return 0;
+	}
+
