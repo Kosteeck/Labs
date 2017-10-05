496cd0f160a701632a9f67faf9d99dcd4732ba0e
Kosteeck <lavrukhinkostya@mail.ru>
2017-10-05 16:55:54 +0300

    Lab1-10

---
+++ Lab1-10/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+/*
+Лаврухин Константин Максимович
+Лабораторная работа №1.
+Вариант №10
+Задание: Перевести из метров в пяди и аршины.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+#include <conio.h>
+#define SPAN 4.37445
+#define ARSH 1.40607424072
+
+
+int main()
+{
+	float meters, arshins, span;
+
+	printf("Please, enter positive number of meters: ");
+	int result = scanf("%f", &meters);
+	if (result == 0) 
+	{
+		printf("Please, enter positive number of meters next time");
+	}
+	else
+	{
+		if (meters >= 0)
+		{
+			arshins = meters * ARSH;
+			printf("\n Your number of meters in arshins: %f", arshins);
+			span = meters * SPAN;
+			printf("\n Your number of meters in spans: %f", span);
+		}
+		else
+		{
+			printf("Please, enter positive number of meters next time");
+		}
+	}
+	_getch();
+	return 0;
+}
