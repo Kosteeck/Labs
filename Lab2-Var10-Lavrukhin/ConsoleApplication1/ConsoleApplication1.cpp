e16f45fca5cd2a10a62c307205ade32794b6c078
Kosteeck <lavrukhinkostya@mail.ru>
2017-10-20 19:23:48 +0300

    Изменения
    Упростил код + добавил проверку на ввод чисел с символами

--- Lab2-Var10-Lavrukhin/ConsoleApplication1/ConsoleApplication1.cpp
+++ Lab2-Var10-Lavrukhin/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,50 +7,50 @@
 */
 
 #define _CRT_SECURE_NO_WARNINGS
-#include "stdafx.h"
 #include "stdio.h"
 #include "conio.h"
-#include "iostream"
-
-int main()
+#define SIZE 100
+int scanf_check();
+int main(void)
 {
-	int Array[100], checkLenght = 0, checkElements, lenght, summEvenNum = 0, j = 0, i, ArithMean;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int Array[SIZE], lenght, summEvenNum = 0, j = 0, i, ArithMean;
 	printf("Please, enter the lenght of the array: ");
-	while (checkLenght == 0)
+	lenght = scanf_check();
+	while (lenght < 0)
 	{
-		checkLenght = scanf("%d", &lenght);
-		if (checkLenght == 0)
-		{
-			while (getchar() != '\n');
-			printf("Please try again and enter a positive number:");
-		}
+		printf("Please try again and enter a positive number: ");
+		lenght = scanf_check();
 	}
 	for (i = 0; i < lenght; i = i + 1)
 	{
-		checkElements = 0;
-		while (checkElements == 0)
+		printf("%d element of array: ", i);
+		Array[i] = scanf_check();
+		if (i % 2 == 0)
 		{
-			printf("%d", i);
-			printf(" element of array: ");
-			checkElements = scanf("%d", &Array[i]);
-			if (checkElements == 0)
-			{
-				while (getchar() != '\n');
-				printf("Please try again and enter a positive number to set array.\n");
-			}
-			else 
-			{
-				if (i % 2 == 0)
-				{
-					summEvenNum = summEvenNum + Array[i];
-					j = j + 1;
-				}
-			}
+			summEvenNum = summEvenNum + Array[i];
+			j = j + 1;
 		}
 	}
 	ArithMean = summEvenNum / j;
-	printf("Arithmetic mean of even elements of the array is: ");
-	printf("%d", ArithMean);
+	printf("\nArithmetic mean of even elements of the array is: %d", ArithMean);
 	_getch();
-	return 0;
+}
+
+int scanf_check()
+{
+	int number;
+	while (!scanf("%d", &number))
+	{
+		while (getchar() != '\n');
+		printf("Please try again and enter a positive number: ");
+	}
+	if (getchar() != '\n')
+	{
+		while (getchar() != '\n');
+		printf("Please try again and enter a positive number: ");
+		number = scanf_check();
+	}
+	return number;
 }
