1da9b94990c385e67733e921e93c626ae39a5ee4
Kosteeck <lavrukhinkostya@mail.ru>
2017-10-21 20:17:56 +0300

    Decorative changes

--- Lab2-Var10-Lavrukhin/ConsoleApplication1/ConsoleApplication1.cpp
+++ Lab2-Var10-Lavrukhin/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,32 +1,35 @@
-	/*
-	Лаврухин Константин Максимович
-	Лабораторная работа №2.
-	Вариант №10
-	Задание:Написать программу, которая вычисляет среднее арифметическое четных элементов массива.
-	Массив и его длина вводятся пользователем.
-	*/
+/*
+Лаврухин Константин Максимович
+Лабораторная работа №2.
+Вариант №10
+Задание:Написать программу, которая вычисляет среднее арифметическое четных элементов массива.
+Массив и его длина вводятся пользователем.
+*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
 #include "conio.h"
+#include "windows.h"
 #define SIZE 100
-	int scanf_check();
-float scanf_check2();
+int scanf_check_int();
+float scanf_check_float();
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	int lenght, j = 0, i;
 	float Array[SIZE], summEvenNum = 0, ArithMean;
 	printf("Please, enter the lenght of the array: ");
-	lenght = scanf_check();
+	lenght = scanf_check_int();
 	while (lenght < 0)
 	{
 		printf("Please try again and enter a positive number: ");
-		lenght = scanf_check();
+		lenght = scanf_check_int();
 	}
 	for (i = 0; i < lenght; i = i + 1)
 	{
 		printf("%d element of array: ", i);
-		Array[i] = scanf_check2();
+		Array[i] = scanf_check_float();
 		if (i % 2 == 0)
 		{
 			summEvenNum = summEvenNum + Array[i];
@@ -34,12 +37,12 @@ int main()
 		}
 	}
 	ArithMean = summEvenNum / j;
-	printf("\nArithmetic mean of even elements of the array is: %f", ArithMean);
+	printf("\nArithmetic mean of even elements of the array is: %1.2f", ArithMean);
 	_getch();
 	return 0;
 }
 
-int scanf_check()
+int scanf_check_int()
 {
 	int number;
 	while (!scanf("%d", &number))
@@ -51,12 +54,12 @@ int scanf_check()
 	{
 		while (getchar() != '\n');
 		printf("Please try again and enter a positive number: ");
-		number = scanf_check();
+		number = scanf_check_int();
 	}
 	return number;
 }
 
-float scanf_check2()
+float scanf_check_float()
 {
 	float number2;
 	while (!scanf("%f", &number2))
@@ -68,7 +71,7 @@ float scanf_check2()
 	{
 		while (getchar() != '\n');
 		printf("Please try again and enter a positive number: ");
-		number2 = scanf_check2();
+		number2 = scanf_check_float();
 	}
 	return number2;
 }
